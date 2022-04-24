f393a0426547a7cf593f62fd9e9bdb401fa3f73f
unknown <pedro.vitor@mail.com>
2022-04-24 16:04:54 -0300

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;300;500;800&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="assets/logo.png">
+
+    <link rel="stylesheet" href="css/pages/login.css">
+
+</head>
+<body>
+    <main class="center">
+        <div class="card">
+            <h1>Cadastro</h1>
+            <form action="" method="post">
+                <input type="email" name="" placeholder="E-mail" id="">
+                <input type="password" name="" placeholder="Senha"  id="">
+                <input type="submit" value="Entrar">
+            </form>
+            <a href="">Esqueceu a senha? Clique aqui.</a>
+            <a href="">Ainda não tem conta? Cadastre-se agora!</a>
+        </div>
+    </main>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem vindo ao Meu Treino!</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100;300;500;800&display=swap" rel="stylesheet">
+
+    <link rel="shortcut icon" href="assets/logo.png">
+
+    <link rel="stylesheet" href="css/pages/welcome.css">
+</head>
+<body>
+    <header class="center">
+        <div class="logo">
+            <img src="assets/logo.png" alt="">
+        </div>
+        <nav>
+            <a href="">Login</a>
+            <a href="">Cadastre-se</a>
+        </nav>
+    </header>
+    <main>
+        <div class="center content">
+            <section class="welcome">
+                <h1>Bem vindo ao meu treino!</h1>
+                <a href="#">Acessar</a>
+            </section>
+        </div>
+        <footer>
+            <a href="https://www.linkedin.com/in/pedro-lima-79732a191/">&copy Pedro Lima Dev. </a>
+        </footer>
+    </main>
+</body>
+</html>
